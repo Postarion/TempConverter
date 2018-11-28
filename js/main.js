cd38e0896e4889ee0cf7ab43e0cf6a6d50050bf6
unknown <d.olszewski21@gmail.com>
2018-11-28 19:50:42 +0100

    Added animated icon

--- js/main.js
+++ js/main.js
@@ -36,3 +36,22 @@ function(e){
         document.getElementById('kevToCel').innerHTML ="";
     }
 });
+
+function tempRise(){
+    let temp = document.getElementById('temp');
+    temp.innerHTML = '&#xf2cb';
+    setTimeout(function(){
+      temp.innerHTML = '&#xf2ca';
+    }, 1000)
+    setTimeout(function(){
+      temp.innerHTML = '&#xf2c9';
+    }, 2000)
+    setTimeout(function(){
+      temp.innerHTML = '&#xf2c8';
+    }, 3000)
+    setTimeout(function(){
+      temp.innerHTML = '&#xf2c7';
+    }, 4000)
+  }
+  tempRise();
+  setInterval(tempRise, 5000);
